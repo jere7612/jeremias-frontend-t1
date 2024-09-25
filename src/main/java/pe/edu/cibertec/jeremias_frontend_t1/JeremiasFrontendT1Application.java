daf42c872480239739087556363533ce3ee19f23
jere7612 <70173572+jere7612@users.noreply.github.com>
2024-09-24 23:15:14 -0500

    Carga Inicial de Evaluacion T1

---
+++ src/main/java/pe/edu/cibertec/jeremias_frontend_t1/JeremiasFrontendT1Application.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.jeremias_frontend_t1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeremiasFrontendT1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JeremiasFrontendT1Application.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/jeremias_frontend_t1/JeremiasFrontendT1ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.jeremias_frontend_t1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeremiasFrontendT1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
