973fe68d5a7cb4ec15660620dc770d8590fbd146
jere7612 <70173572+jere7612@users.noreply.github.com>
2024-09-26 23:46:19 -0500

    Implementacion html prueva y resultado

--- src/main/java/pe/edu/cibertec/jeremias_frontend_t1/controller/AutoController.java
+++ src/main/java/pe/edu/cibertec/jeremias_frontend_t1/controller/AutoController.java
@@ -1,5 +1,4 @@
 package pe.edu.cibertec.jeremias_frontend_t1.controller;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,8 +11,7 @@ import pe.edu.cibertec.jeremias_frontend_t1.dto.Auto;
 @RequestMapping("/login")
 public class AutoController {
 
-
-    private final String API_URL = "http://localhost:8081/api/auto/buscar";
+    private final String API_URL = "http://localhost:8081/api/auto/buscar/";
     private final RestTemplate restTemplate;
 
     public AutoController(RestTemplate restTemplate) {
@@ -22,19 +20,28 @@ public class AutoController {
 
     @GetMapping("/inicio")
     public String mostrarFormulario(Model model) {
-        return "inicio";
+        model.addAttribute("mensaje", null);
+        return "prueba";
     }
+
+
     @GetMapping("/buscar-auto")
     public String buscarAuto(@RequestParam("placa") String placa, Model model) {
-        // Consumir el API REST
-        Auto auto = restTemplate.getForObject(API_URL + "?placa=" + placa, Auto.class);
+        try {
+            // Consumir el API REST
+            Auto auto = restTemplate.getForObject(API_URL + placa, Auto.class);
 
-        if (auto != null) {
-            model.addAttribute("auto", auto);
-            return "resultado";
-        } else {
-            model.addAttribute("mensaje", "Auto no encontrado");
-            return "resultado";
+
+            if (auto != null) {
+                model.addAttribute("auto", auto);
+                return "resultado";
+            } else {
+                model.addAttribute("mensaje", "No se encontró un vehículo para la placa ingresada");
+                return "prueba";
+            }
+        } catch (Exception e) {
+            model.addAttribute("mensaje", "No se encontró un vehículo para la placa ingresada");
+            return "prueba";
         }
     }
 }
